46c5a1a837a5268f5dc06484ef8c01fa4eabe7e4
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-18 10:05:11 +0530

    print char XML in reverse way

---
+++ assignment no6.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char num1= 'X';
+	char num2= 'M';
+	char num3 ='L';
+	printf("the reverse of %c%c%c is%c%c%C\n",
+	     
+		   num1,num2,num3,
+		   num3,num2,num1);
+}
