187b2ecbd4886a6374fce1cf3840a6d35186c4a5
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-28 11:34:46 +0530

    string inbuild 5(strcat)

---
+++ string.h5(cat).cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char name1[20]={"payal"};
+	char name2[20]={"raj"};
+	int cat;
+	
+    strcat(name1,name2);
+	printf("%s",name1);
+	getch();
+}
