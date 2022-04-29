13ce9d469c7de9af4c0380b9a6c1d593e3e1287d
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-29 14:32:01 +0530

    n digit no.palindrome or not (for loop).cpp

---
+++ n digit no.palindrome or not (for loop).cpp
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main()
+{
+	int n,bak,num,rev=0;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	bak=n;
+	for(;n>0;)
+	{
+		num=n%10;
+		rev=rev*10+num;
+		n=n/10;
+	}
+	if(bak==rev)
+	{
+		printf("palindrome number");
+	}
+	else
+	{
+		printf("not a palindrome number");
+	}
+	
+}
