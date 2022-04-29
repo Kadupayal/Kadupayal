abb5a4ca7b0cf2f5e14c825735cfcd1071cb0838
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-29 11:10:07 +0530

    wac program to enter x and y from keyboard & find the power

---
+++ while loop 10.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,y,i,pow=1;
+	printf("enter x and y values");
+	scanf("%d%d",&x,&y);
+	
+	i=1;
+	while(i<=y)
+	{
+		pow=pow*x;
+		i++;
+	}
+	printf("pow=%d",pow);
+}
