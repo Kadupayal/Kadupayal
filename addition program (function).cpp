0c7a093d2b8ca9be6552a86c873e3c8f111d8420
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-29 15:58:58 +0530

    Add files via upload

---
+++ addition program (function).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int add();
+int main()
+{
+	add();
+}
+	int add()
+	{
+	int num1,num2,res;
+	printf("enter two number");
+	scanf("%d%d",&num1,&num2);
+	res=num1+num2;
+	printf("%d",res);
+    }
+	
+
