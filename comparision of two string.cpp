2f77b12965840b349f5ad8a5e5cd7501a245ac98
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-29 21:07:35 +0530

    comparision of two string

---
+++ comparision of two string.cpp
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char name1[15]={"payal"};
+	char name2[15]={"payal"};
+	if(strcmp(name1,name2)==0)
+	{
+		printf("string name1 and string name2 are same");
+	}
+	else
+	{
+	printf("string name1 and string name2 are not same");	
+	}
+
+}
