ca32ef7301c592f3ce4a73c7219c4a1ed8a852d6
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-29 21:15:41 +0530

    concatnates of string like an array(cat)

---
+++ concatnates of string like an array(cat).cpp
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	char ch1[20]={"payal"};
+	char ch2[20]={"revati"};
+	for(i=0;ch1[i]!='\0';i++)
+	{
+	printf("%c",ch1[i]);	
+	}
+	for(i=0;ch2[i]!='\0';i++)
+	{
+	printf("%c",ch2[i]);	
+	}	
+}
