f2bf13f9b52b4337f53bb943894f3ebbcdc7f629
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-04-29 16:00:43 +0530

    Add files via upload

---
+++ division program(function).cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int div();
+int main()
+{
+	div();
+}
+	int div()
+	{
+	int num1,num2,res;
+	printf("enter two number");
+	scanf("%d%d",&num1,&num2);
+	res=num1/num2;
+	printf("%d",res);
+    }
