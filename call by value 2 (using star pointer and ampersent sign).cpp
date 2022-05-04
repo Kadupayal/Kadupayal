80b39bf8d31339aa221b5023fac2e5aa1b1e8d6f
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-04 17:38:42 +0530

    call by value 2 (using star pointer and ampersent sign)(function calling technique)

---
+++ call by value 2 (using star pointer and ampersent sign).cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void fun(int *x, int *y);
+int main()
+{
+	int a=2, b=3;
+	printf("a=%d b=%d",a,b);
+	fun(&a,&b);
+	printf("a=%d b=%d",a,b);
+	
+}
+void fun(int *x,int *y)
+{
+	*x=12;
+	*y=13;
+}
