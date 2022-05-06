f279f3bdfdb870aed6ff848a4eab6bed6d7ce845
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-06 11:50:45 +0530

    wac program to check 3 given int & return true if one of them is 20 or more less than one of the others

--- assignment 97.cpp
+++ assignment 97.cpp
@@ -5,9 +5,9 @@ int main()
 	int x,y,z;
 	printf("enter three given number\n");
 	scanf("%d%d%d",&x,&y,&z);
-	if((y>x) && (z>y))
-	{
-		printf("true");
+    if((x-y>=20) || (y-z>=20) || (z-x>=20))
+    {
+    	printf("true");
 	}
 	else
 	{
