891dc82f60b777069d3ee9f9dd92f1ebbb0d344b
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-09 12:04:20 +0530

    wac program to print the res of specified operations. -1+4*6        (35+5)%7      14+ -4*6/11     2+15/6*1-7%2

---
+++ assignment 72.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a1,b1,c1,d1;
+a1=-1+4*6;
+b1=(35+5)%7;
+c1=14+(-4)*6/11;
+d1=2+15/6*1-7%2;
+
+printf("a1= %d b1= %d c1=%d d1=%d",a1,b1,c1,d1);
+getch();
+}
