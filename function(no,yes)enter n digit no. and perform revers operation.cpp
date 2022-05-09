3cd1e3b34798e441c0a28bd2a1abc5ab30a5fdd4
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-09 11:36:35 +0530

    no argument with returntype(enter n digit no. and perform revers operation.)

--- function(no,yes)enter n digit no. and perform revers operation.cpp
+++ function(no,yes)enter n digit no. and perform revers operation.cpp
@@ -1,23 +1,28 @@
+
 #include<stdio.h>
-#include<stdio.h>
-int rev()
-{
-	int num,rev=0,n;
-	printf("enter the number: ");
-	scanf("%d",&n);
-	for(;n>0;)
-	{
-		num=n%10;
-		rev=rev*10+num;
-		n=n/10;
-	}
-	return rev;
-	
-}
+#include<conio.h>
+int rev();
 int main()
 {
-	int r;
-	r=rev();
-	printf("reverse= %d",r);
+int r;
+ r=rev();
+printf("reverse =%d",r);
+getch();
 }
+int rev()
+{
+int res=0,i,b,m,a;
+printf("enter the number ");
+scanf("%d",&a);
 
+while(a>=1)
+{
+b=a%10;
+res=res*10+b;
+a=a/10;
+ printf("%d\n",res);
+}
+m=res;
+return m;
+
+}
