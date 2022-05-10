2261638d2adc814ae6f2a6549812d990c9444aba
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-10 20:46:53 +0530

    union_union stud in main fun(student information)

---
+++ union_union stud in main fun(student information).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+union stud
+{
+  int id;
+  char name[20];
+};
+int main()
+{
+	union stud s1;
+	printf("enter student id\n");
+	scanf("%d",&s1.id);
+	
+	printf("enter student name\n");
+	scanf("%s",s1.name);
+	
+	printf("student id=%d\n",s1.id);
+	printf("student name=%s",s1.name);
+}
