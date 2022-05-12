c6f8d4db36764ec3041cafe78ecee6471faee62c
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-12 10:50:21 +0530

    Add files via upload

---
+++ pattern 17.cpp
@@ -0,0 +1,22 @@
+/*EEEEE
+  DDDD
+  CCC
+  BB
+  A
+  */
+#include<stdio.h>
+int main()
+{
+	int r,c;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{
+			printf("%c",r+64);
+		
+		}
+		printf("\n");
+	}
+}  
+
+
