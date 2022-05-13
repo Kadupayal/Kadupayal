5d20bfb485e927266c528929e82aca9d981a7a35
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-13 12:03:56 +0530

    check a given array of integers and return true if there is a 3 with a 5 somewhere later in the given array

---
+++ assignment 128.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[6],size,num1;
+	printf("enter the size of array: ");
+	scanf("%d",&size);
+	printf("enter the array elements\n");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		if(a[i]==3 && a[i+1]==5)
+		num1=1;
+	}
+	if(num1==1)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+		
+	}
+}
