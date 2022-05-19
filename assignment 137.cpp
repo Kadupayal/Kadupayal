b48c608860f4e1f15799666ec4a04f206bc5f575
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-05-19 21:00:13 +0530

    create a new array from a gien array of integers shifting all zeros to left direction

---
+++ assignment 137.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,x=0,a[10],b[10];
+	printf("enter array size: ");
+	scanf("%d",&n);
+	printf("enter array element\n");\
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==0)
+		{
+			b[x]=0;
+			x=x+1;
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]!=0)
+		{
+		b[x]=a[i];
+		x=x+1;	
+		}	
+	}
+	printf("new array\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n",b[i]);
+	}
+}
