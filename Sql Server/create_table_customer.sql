c50779746e1201c70c1c8994c971b194575d6138
Kadupayal <102897976+Kadupayal@users.noreply.github.com>
2022-10-01 22:00:38 +0530

    create table customer

---
+++ Sql Server/create_table_customer.sql
@@ -0,0 +1,24 @@
+
+
+create table customer(custid int primary key,custname varchar(20))
+
+
+insert into customer values (1,'amit')
+select  * from customer
+
+insert into customer values (1,'amit')
+select * from customer
+
+insert into customer values (1,'amit')
+
+
+
+
+
+
+
+
+
+
+
+
